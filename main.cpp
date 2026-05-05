18afac25645c0d374831724d50609a80f619b07e
evynnevy <zhangyiwen0626@icloud.com>
2026-05-05 21:56:31 +0800

    v2.0
    新增：宠物选择+全局bgm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 #include "PetStatus.h"
+#include "PetSelectionModel.h"
+#include "MusicPlayer.h"
 
 int main(int argc, char *argv[])
 {
@@ -8,7 +11,15 @@ int main(int argc, char *argv[])
 
     qmlRegisterType<PetStatus>("com.petlife", 1, 0, "PetStatus");
 
+    PetSelectionModel petModel;
+    MusicPlayer musicPlayer;
+
+    musicPlayer.setSource("qrc:/audio/bgm.mp3");
+    musicPlayer.play();
+
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("petModel", &petModel);
+    engine.rootContext()->setContextProperty("musicPlayer", &musicPlayer);
     engine.load(QUrl(QStringLiteral("qrc:/Main.qml")));
 
     if (engine.rootObjects().isEmpty())

f7a9078c46e100c04f5a600c0891d7f4bf922fef
evynnevy <zhangyiwen0626@icloud.com>
2026-05-06 20:45:44 +0800

    v3.2
    新增：互动的时候福瑞发出汪汪喵喵声

--- main.cpp
+++ main.cpp
@@ -5,24 +5,28 @@
 #include "PetSelectionModel.h"
 #include "MusicPlayer.h"
 #include "SaveManager.h"
+#include "SoundEffectPlayer.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     QGuiApplication app(argc, argv);
 
     qmlRegisterType<PetStatus>("com.petlife", 1, 0, "PetStatus");
 
-    //v2.0:新增：宠物选择
+    //v2.0 新增：宠物选择
     PetSelectionModel petModel;
 
-    //v2.0：新增全局bgm
+    //v2.0 新增：全局bgm
     MusicPlayer musicPlayer;
     musicPlayer.setSource("qrc:/audio/bgm.mp3");
     musicPlayer.play();
 
-    //v3.0:新增存档读档系统
+    //v3.0 新增：存档读档系统
     PetStatus globalPetStatus;
     SaveManager saveManager(&globalPetStatus, &petModel, &musicPlayer);
+
+    // v3.2 新增：猫叫狗叫音效
+    SoundEffectPlayer soundEffectPlayer;
+    globalPetStatus.setSoundEffectPlayer(&soundEffectPlayer);
 
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("petModel", &petModel);

afc990e6476a2650008eb8b9c9fcd8757a8f5f03
evynnevy <zhangyiwen0626@icloud.com>
2026-05-06 08:33:42 +0800

    v3.0
    修复bug：选择宠物后无法显示各动物独特属性；
    新增：存档读档系统，优化游戏体验。

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "PetStatus.h"
 #include "PetSelectionModel.h"
 #include "MusicPlayer.h"
+#include "SaveManager.h"
 
 int main(int argc, char *argv[])
 {
@@ -11,17 +12,25 @@ int main(int argc, char *argv[])
 
     qmlRegisterType<PetStatus>("com.petlife", 1, 0, "PetStatus");
 
+    //v2.0:新增：宠物选择
     PetSelectionModel petModel;
-    MusicPlayer musicPlayer;
 
+    //v2.0：新增全局bgm
+    MusicPlayer musicPlayer;
     musicPlayer.setSource("qrc:/audio/bgm.mp3");
     musicPlayer.play();
+
+    //v3.0:新增存档读档系统
+    PetStatus globalPetStatus;
+    SaveManager saveManager(&globalPetStatus, &petModel, &musicPlayer);
 
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("petModel", &petModel);
     engine.rootContext()->setContextProperty("musicPlayer", &musicPlayer);
-    engine.load(QUrl(QStringLiteral("qrc:/Main.qml")));
+    engine.rootContext()->setContextProperty("petStatus", &globalPetStatus);
+    engine.rootContext()->setContextProperty("saveManager", &saveManager);
 
+    engine.load(QUrl(QStringLiteral("qrc:/Main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
 
